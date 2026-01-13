72f4d3dbd93334d936edce96038378683e1609db
Célian CHAUSSON <celian.chau@gmail.com>
2026-01-13 16:54:35 +0100

    fix test

--- src/test/aggregateInMemoryIntegration.spec.ts
+++ src/test/aggregateInMemoryIntegration.spec.ts
@@ -40,14 +40,14 @@ describe('aggregateInMemoryIntegration', () => {
 
     it('should aggregate political opinions per game type', async () => {
         const userData: User[] = [
-            new User(25, 'M', 'Engineer', 'Reconquête', 'French', ['Game1', 'Game2'], 1),
-            new User(30, 'F', 'Unemployed', 'RN', 'French', ['Game1'], 2),
-            new User(22, 'M', 'Student', 'Reconquête', 'French', ['Game1'], 3),
-            new User(28, 'F', 'Teacher', 'Ecologie Les Verts', 'French', ['Game2'], 4),
-            new User(35, 'M', 'Doctor', 'Reconquête', 'French', ['Game2', 'Game1'], 5),
-            new User(40, 'F', 'Lawyer', 'Ecologie Les Verts', 'French', ['Game1'], 6),
-            new User(27, 'M', 'Artist', 'RN', 'French', ['Game2', 'Game1'], 7),
-            new User(32, 'F', 'Engineer', 'LFI', 'French', ['Game2'], 8),
+            new User(25, 'M', 'Engineer', 'Reconquête', 'French', ['Game1', 'Game2']),
+            new User(30, 'F', 'Unemployed', 'RN', 'French', ['Game1']),
+            new User(22, 'M', 'Student', 'Reconquête', 'French', ['Game1']),
+            new User(28, 'F', 'Teacher', 'Ecologie Les Verts', 'French', ['Game2']),
+            new User(35, 'M', 'Doctor', 'Reconquête', 'French', ['Game2', 'Game1']),
+            new User(40, 'F', 'Lawyer', 'Ecologie Les Verts', 'French', ['Game1']),
+            new User(27, 'M', 'Artist', 'RN', 'French', ['Game2', 'Game1']),
+            new User(32, 'F', 'Engineer', 'LFI', 'French', ['Game2']),
         ];
         const gameData: Game[] = [new Game(18, "Game1", "Action", true, false, true), new Game(12, "Game2", "Adventure", false, true, false)];
         userData.forEach(user => {
